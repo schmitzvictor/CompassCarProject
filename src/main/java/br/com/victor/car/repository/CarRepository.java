0cecb3c39122a95db4c6a8bfbf26e994af2dfba3
Victor Schmitz <victorsgm2003@hotmail.com>
2023-07-16 13:23:52 -0300

    Fixed repository

--- src/main/java/br/com/victor/car/repository/CarRepository.java
+++ src/main/java/br/com/victor/car/repository/CarRepository.java
@@ -4,9 +4,8 @@ import br.com.victor.car.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
-    Optional<Car> findByIdChassi(Long idChassi);
+
 }
